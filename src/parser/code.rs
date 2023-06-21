ff1f3aa178cae838f8088603d122cf01e3fa6cc8
afpro <admin@afpro.net>
2023-06-22 02:41:56 +0800

    with some test code

--- src/parser/code.rs
+++ src/parser/code.rs
@@ -1,3 +1,24 @@
 #[derive(pest_derive::Parser)]
 #[grammar = "parser/code.pest"]
 pub struct Code;
+
+#[cfg(test)]
+mod test {
+    use {
+        super::*,
+        pest::{iterators::Pairs, Parser},
+    };
+
+    #[test]
+    fn parse() {
+        let root = Code::parse(Rule::root, " #with comment\n.class").expect("parse");
+        dump_pair(root, "");
+    }
+
+    fn dump_pair<S: AsRef<str>>(pairs: Pairs<Rule>, prefix: S) {
+        for pair in pairs {
+            println!("{}pair {:?}", prefix.as_ref(), pair.as_rule());
+            dump_pair(pair.into_inner(), format!("  {}", prefix.as_ref()))
+        }
+    }
+}
